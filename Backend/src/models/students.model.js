ae83576f00263e19e08667e7fae885303e3f7d89
Vishnu Raghav <vishnur1225@gmail.com>
2025-08-15 12:17:43 -0700

    changes in models

--- Backend/src/models/students.model.js
+++ Backend/src/models/students.model.js
@@ -1,4 +1,7 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+
 
 const studentSchema = new mongoose.Schema(
   {
@@ -42,8 +45,22 @@ const studentSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    refreshToken : {
+      type : String
+    }
   },
   { timestamps: true }
 );
+
+studentSchema.pre("save",async function(next){
+  if(!this.isModified("password")) return next()
+  this.password = bcrypt.hash(this.password,10)
+  next()
+})
+
+studentSchema.methods.isPasswordCorrect = async function(password){
+  return await bcrypt.compare(password,this.password)
+
+}
 
 export const Student = mongoose.model("Student", studentSchema);
