168b65d34590347448cb76ee91a877585e4d6013
Vishnu Raghav <vishnur1225@gmail.com>
2025-08-15 08:20:48 -0700

    centralized error handling with ApiError and asyncHandler

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,10 +1,21 @@
-import mongoose from "mongoose"
-import {DB_NAME} from "./constants.js"
 import connectDB from "./db/index.js"
+import dotenv from "dotenv"
+import {app} from "./app.js"
+
+dotenv.config({ path : "./env"})
+
 
 
 
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000 , () => {
+        console.log(`Server is running at PORT :${process.env.PORT} `)
+    })
+})
+.catch((error) => {
+    comsole.log("MONGODB CONNECTION FAILED :",error)
+})
 
 
 
