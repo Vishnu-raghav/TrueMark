dd1997501e62087a5985e3ba449bd04ef98f18c2
Vishnu Raghav <vishnur1225@gmail.com>
2025-08-17 03:37:14 -0700

    feat: add role-based authentication support

--- Backend/src/models/user.model.js
+++ Backend/src/models/user.model.js
@@ -65,3 +65,32 @@ userSchema.methods.isPasswordCorrect = async function (password) {
   return await bcrypt.compare(password, this.password);
 };
 
+userSchema.methods.generateAccessToken = function(){
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      rollNo: this.rollNo,
+      name: this.name,
+      role: this.role,  
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY, 
+    }
+  )
+}
+
+userSchema.methods.generateRefreshToken = function(){
+  return jwt.sign(
+    {
+      _id : this._id,
+      email : this.email
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn : process.env.REFRESH_TOKEN_EXPIRY
+    }
+  )
+}
+
