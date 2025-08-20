5c0621053ee2c722b155514c9efd9075b0af736f
Vishnu Raghav <vishnur1225@gmail.com>
2025-08-20 11:38:04 -0700

    write register controller

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -13,5 +13,25 @@ app.use(express.urlencoded({extended : true , limit : "16Kb"}))
 app.use(express.static("public"))
 
 app.use(cookieParser())
+
+// import routes
+
+import userRouter from "./routes/user.routes.js"
+
+
+// route declaration  
+
+app.use("/api/v1/users",userRouter)
+
+// https://localhost:8000/users/login
+
+
+
+
+
+
+
 export {app}
+
+
 
