146deb071c026f1a6236657c5a861da0e9c880e5
Vishnu Raghav <vishnur1225@gmail.com>
2025-09-24 21:26:00 -0700

    Remove unused imports and clean up whitespace in App component

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -8,7 +6,7 @@ function App() {
 
   return (
     <>
-      <h1 className='bg-amber-400 '>vishnu</h1>
+      <h1 className='bg-amber-400'>vishnu</h1>
     </>
   )
 }
