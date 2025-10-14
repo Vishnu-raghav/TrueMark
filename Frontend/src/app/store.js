a7219a8a75d04588594916fd6dd2c8f9d2472012
Vishnu Raghav <vishnur1225@gmail.com>
2025-10-14 11:00:12 -0700

    feat: add Redux store configuration with auth, certificate, and organization reducers

---
+++ Frontend/src/app/store.js
@@ -0,0 +1,13 @@
+import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "../features/auth/authslice.js";
+import certificateReducer from "../features/certificates/certificateSlice.js";
+import organizationReducer from "../features/organization/organizationSlice.js";
+
+export const store = configureStore({
+  reducer: {
+    auth: authReducer,
+    certificate: certificateReducer,
+    organization: organizationReducer,
+  },
+  devTools: true, 
+});
